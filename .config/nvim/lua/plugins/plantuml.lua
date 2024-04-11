4defc5dc913ce6f7ee0934c8df019affc1172e6d
Roger Ramos <rogrp6@gmail.com>
2024-04-11 01:18:38 -0500

    plantuml plugin

---
+++ .config/nvim/lua/plugins/plantuml.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "https://gitlab.com/itaranto/plantuml.nvim",
+    version = "*",
+    opts = {
+      renderer = {
+        type = "imv", -- 'image', 'text', 'imv'
+      },
+    },
+    config = true,
+  },
+}
