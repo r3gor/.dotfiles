45d81fa1b97534fa75baaaca5b46a127d49565e2
Roger Ramos <rogrp6@gmail.com>
2024-07-18 06:17:06 -0500

    add plugings

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -232,4 +232,26 @@ return {
       },
     },
   },
+  {
+    "Bekaboo/deadcolumn.nvim",
+    config = true,
+    opts = {
+      scope = "buffer",
+      blending = {
+        threshold = 0.75,
+        colorcode = "#FFFFFF",
+        hlgroup = { "Normal", "bg" },
+      },
+      warning = {
+        alpha = 0.4,
+        offset = 0,
+        colorcode = "#FF0000",
+        hlgroup = { "Error", "bg" },
+      },
+      extra = {
+        ---@type string?
+        follow_tw = nil,
+      },
+    },
+  },
 }
