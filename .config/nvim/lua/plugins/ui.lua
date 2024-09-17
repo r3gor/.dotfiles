e91db87045988228ab1f3d509d2fb1f212d02dc9
Roger Ramos <rogrp6@gmail.com>
2024-09-17 11:12:23 -0500

    nvim ui incline diagnostic label

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -137,13 +137,13 @@ return {
         local modified = vim.bo[props.buf].modified
 
     local function get_diagnostic_label()
-        local icons = { error = '', warn = '', info = '', hint = '' }
+        local icons = { error = ' ', warn = ' ', info = ' ', hint = '󰛨 ' }
         local label = {}
 
         for severity, icon in pairs(icons) do
             local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
             if n > 0 then
-                table.insert(label, { icon .. ' ' .. n .. ' ', group = 'DiagnosticSign' .. severity })
+                table.insert(label, { icon .. '' .. n .. ' ', group = 'DiagnosticSign' .. severity })
             end
         end
         if #label > 0 then
--- .zshrc
+++ .zshrc
@@ -81,13 +81,13 @@ export stk=~/Personal/Softtek
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/home/rogrp/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
-  eval "$__conda_setup"
+    eval "$__conda_setup"
 else
-  if [ -f "/home/rogrp/anaconda3/etc/profile.d/conda.sh" ]; then
-    . "/home/rogrp/anaconda3/etc/profile.d/conda.sh"
-  else
-    export PATH="/home/rogrp/anaconda3/bin:$PATH"
-  fi
+    if [ -f "/home/rogrp/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/rogrp/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/rogrp/anaconda3/bin:$PATH"
+    fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
