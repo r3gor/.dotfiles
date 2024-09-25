353f0c4c308ed9b125136349bed672fc880fdbf2
Roger Ramos <rogrp6@gmail.com>
2024-09-25 00:09:01 -0500

    nvim: paste img plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -349,4 +349,15 @@ return {
   -- {
   --   "dosimple/workspace.vim",
   -- },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    keys = {
+      { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+    },
+  }
 }
